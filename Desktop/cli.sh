60a6bdc917feada5e14cc9dd3f5da26359329767
Alvaro Fimpel <afimpel@gmail.com>
2023-01-24 00:51:58 -0300

    cli installer

---
+++ Desktop/cli.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apk add flatpak
+mkdir -p /usr/share/pixmaps/
+mkdir -p /usr/share/applications/
+
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/pkexec_upgrade-system > /usr/bin/pkexec_upgrade-system
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/pkexec_upgrade-system-poweroff > /usr/bin/pkexec_upgrade-system-poweroff
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/pkexec_upgrade-system-reboot > /usr/bin/pkexec_upgrade-system-reboot
+
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/gksu_upgrade.desktop > /usr/share/applications/gksu_upgrade.desktop
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/gksu_poweroff.desktop > /usr/share/applications/gksu_poweroff.desktop
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/gksu_reboot.desktop > /usr/share/applications/gksu_reboot.desktop
+
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/upgrade.png > /usr/share/pixmaps/upgrade.png
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/shutdown.png > /usr/share/pixmaps/shutdown.png
+curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/desktop/Desktop/restart.png > /usr/share/pixmaps/restart.png
+
+chmod 777 /usr/share/applications/gksu_*
+chmod 777 /usr/share/pixmaps/* -R
+chmod 777 /usr/bin/pkexec_upgrad* -R
