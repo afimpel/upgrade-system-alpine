99bf7b9ba44dcaddc4133e51dfdd4c4bc11c8c2b
afimpel <afimpel@afimpel.com>
2024-04-28 22:31:33 -0300

    adduser wheel

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ apk add nano git mc bash zsh htop neofetch curl btop sudo
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 echo "%wheel ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown, /usr/bin/upgrade-syste*" >> /etc/sudoers.d/wheel
 sed -i 's/\/bin\/ash/\/bin\/bash/g' /etc/passwd
+adduser $(getent passwd 1000 | cut -d: -f1) wheel
 curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/main/upgrade-system > /usr/bin/upgrade-system
 curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/main/upgrade-system-poweroff > /usr/bin/upgrade-system-poweroff
 curl -L https://raw.githubusercontent.com/afimpel/upgrade-system-alpine/main/upgrade-system-reboot > /usr/bin/upgrade-system-reboot
